8123f961ca5129535aa0bc36dbbd1bc1bb2c1077
AchuanAchuan <125677144+AchuanAchuan@users.noreply.github.com>
2024-09-09 17:13:33 +0800

    Add files via upload

--- example/cpp/stand_go2free.cpp
+++ example/cpp/stand_go2free.cpp
@@ -37,17 +37,16 @@ private:
     void InitLowCmd();
     void LowStateMessageHandler(const void *messages);
     void LowCmdWrite();
-    void calcPb0Psi();
-    Vec34 getQ();
-    Vec34 getVecXP();
-    Vec34 _calcOP(float row, float pitch, float yaw, float height);
-    Vec12 getQif(const Vec34 &vecP, FrameType frame);
+ 
     Vec12 Freestandcmd(float row, float pitch, float yaw, float height);
     
 
 private:
-    double stand_up_joint_pos[12];
-    double stand_down_joint_pos[12];
+
+    double stand_up_joint_pos[12] = {0.00571868, 0.608813, -1.21763, -0.00571868, 0.608813, -1.21763,
+                                     0.00571868, 0.608813, -1.21763, -0.00571868, 0.608813, -1.21763};
+    double stand_down_joint_pos[12] = {0.0473455, 1.22187, -2.44375, -0.0473455, 1.22187, -2.44375, 0.0473455,
+                                       1.22187, -2.44375, -0.0473455, 1.22187, -2.44375};
     double dt = 0.002;
     double runing_time = 0.0;
     double phase = 0.0;
@@ -55,16 +54,11 @@ private:
     unitree::robot::go2::RobotStateClient rsc;
 
     QuadrupedLeg* _Legs[4];
-    Vec34 qLegs;
-    Vec34 qLegsfree;
+
     float Pz = 0.37;
     int freemode = 0;
-    Vec3 pDesRef;
-    Vec3 pDes;
-    Vec3 qLegsdes;
-    Vec3 qLegsdesRef;
-    Vec3 _initVecOX;
-    Vec34 _initVecXP;
+    int set_freemode = 0;
+
     Vec12 freeQ;
 
     unitree_go::msg::dds_::LowCmd_ low_cmd{};     // default init
@@ -148,70 +142,30 @@ void Custom::InitLowCmd()
     _Legs[2] = new Go2Leg(2, Vec3(-0.1934, -0.0465, 0));
     _Legs[3] = new Go2Leg(3, Vec3(-0.1934,  0.0465, 0));
 
-    pDesRef[0] = 0.0;
-    pDesRef[1] = -0.0885;
-    pDesRef[2] = -0.14999;
-    qLegsdesRef = _Legs[0]->calcQ(pDesRef , FrameType::HIP);
-    std::cout << "qLegsdesRef:" << qLegsdesRef[0] <<" "<< qLegsdesRef[1] <<" "<< qLegsdesRef[2] << endl;
-
-    //std::cout << "inputS:Pz"<< endl;
-    //std::cin >> Pz; 
     std::cout << "X(ROLL):0;Y(YAW):1;Z(PITCH):2;Height:3;MIx:4"<< endl;
     std::cin >> freemode; 
-    pDes[0] = 0.0;
-    pDes[1] = -0.0935;
-    pDes[2] = -Pz;
-    qLegsdes = _Legs[0]->calcQ(pDes , FrameType::HIP);
-    std::cout << "qLeg0des:" << qLegsdes[0] <<" "<< qLegsdes[1] <<" "<< qLegsdes[2] <<endl;
-
-    for(int i = 0;i < 12;i++){
-        if(i == 3 || i == 9){
-            stand_down_joint_pos[i] = -qLegsdesRef[i%3];
-        }
-        else{
-            stand_down_joint_pos[i] = qLegsdesRef[i%3];     
-        }
-    }
-    for(int i = 0;i < 12;i++){
-        if(i == 3 || i == 9){
-            stand_up_joint_pos[i] = -qLegsdes[i%3];
-        }
-        else{
-            stand_up_joint_pos[i] = qLegsdes[i%3];     
-        }
-    }
-    // stand_down_joint_pos[12] = {qLegsdesRef[0], qLegsdesRef[1], qLegsdesRef[2], -qLegsdesRef[0], qLegsdesRef[1], qLegsdesRef[2],
-    //                                  qLegsdesRef[0], qLegsdesRef[1], qLegsdesRef[2], -qLegsdesRef[0], qLegsdesRef[1], qLegsdesRef[2]};
-    // stand_up_joint_pos[12] = {qLegsdes[0], qLegsdes[1], qLegsdes[2], -qLegsdes[0], qLegsdes[1], qLegsdes[2],
-    //                                  qLegsdes[0], qLegsdes[1], qLegsdes[2], -qLegsdes[0], qLegsdes[1], qLegsdes[2]};
+    std::cout << "freemode:" << freemode << endl;
 }
-Vec34 Custom::getQ(){
-        Vec34 qLegs;
-        for(int i(0); i < 4; ++i){
-            qLegs.col(i)(0) = low_state.motor_state()[3*i].q();
-            qLegs.col(i)(1) = low_state.motor_state()[3*i + 1].q();
-            qLegs.col(i)(2) = low_state.motor_state()[3*i + 2].q();
-        }
-        return qLegs;
-    }
 
-Vec34 Custom::getVecXP(){
-    Vec3 x = _Legs[0]->calcPEe2B(getQ().col(0));
-    Vec34 vecXP, qLegs;
-    qLegs = getQ();
-
+Vec12 Custom::Freestandcmd(float row, float pitch, float yaw, float height){
+    Vec34 _initVecXP, qLegs;
+    Vec3 _initVecOX;
     for(int i(0); i < 4; ++i){
-        vecXP.col(i) = _Legs[i]->calcPEe2B(qLegs.col(i)) - x;
+         qLegs.col(i)(0) = low_state.motor_state()[3*i].q();
+         qLegs.col(i)(1) = low_state.motor_state()[3*i + 1].q();
+         qLegs.col(i)(2) = low_state.motor_state()[3*i + 2].q();
+     }
+    Vec3 x = _Legs[0]->calcPEe2B(qLegs.col(0));
+    _initVecOX = _Legs[0]->calcPEe2B(qLegs.col(0));
+    
+    for(int i(0); i < 4; ++i){
+        _initVecXP.col(i) = _Legs[i]->calcPEe2B(qLegs.col(i)) - x;
     }
-    return vecXP;
-}
 
-void Custom::calcPb0Psi(){
-    _initVecOX = _Legs[0]->calcPEe2B(getQ().col(0));
-    _initVecXP = getVecXP();
-}
+    Vec34 vecPsi; 
+    Vec12 qDes;
+    FrameType BODY;
 
-Vec34 Custom::_calcOP(float row, float pitch, float yaw, float height){
     Vec3 vecXO = -_initVecOX;
     vecXO(2) += height;
 
@@ -226,25 +180,14 @@ Vec34 Custom::_calcOP(float row, float pitch, float yaw, float height){
         tempVec4 = Tbs * homoVec(_initVecXP.col(i));
         vecOP.col(i) = noHomoVec(tempVec4);
     }
+    vecPsi = vecOP;
 
-    return vecOP;
-}
-
-Vec12 Custom::getQif(const Vec34 &vecP, FrameType frame){
     Vec12 q;
     for(int i(0); i < 4; ++i){
-        q.segment(3*i, 3) = _Legs[i]->calcQ(vecP.col(i), frame);
+        q.segment(3*i, 3) = _Legs[i]->calcQ(vecPsi.col(i), BODY);
     }
-    return q;
-}
+    qDes = q;
 
-Vec12 Custom::Freestandcmd(float row, float pitch, float yaw, float height){
-    calcPb0Psi();
-    Vec34 vecPsi; 
-    Vec12 qDes;
-    FrameType BODY;
-    vecPsi = _calcOP(row, pitch, yaw, height);
-    qDes = getQif(vecPsi, BODY);
     return qDes;
 }
 
@@ -281,13 +224,13 @@ void Custom::LowCmdWrite()
             low_cmd.motor_cmd()[i].q() = phase * stand_up_joint_pos[i] + (1 - phase) * stand_up_joint_pos[i];
             low_cmd.motor_cmd()[i].dq() = 0;
             if(i%3 == 0){
-                low_cmd.motor_cmd()[i].kp() = 50;
+                low_cmd.motor_cmd()[i].kp() = 55;
             }
             else if(i%3 == 1){
-                low_cmd.motor_cmd()[i].kp() = 40;
+                low_cmd.motor_cmd()[i].kp() = 55;
             }
             else if(i%3 == 2){
-                low_cmd.motor_cmd()[i].kp() = 80;
+                low_cmd.motor_cmd()[i].kp() = 85;
             }
             low_cmd.motor_cmd()[i].kd() = 5;
             if(i%3 == 2){
@@ -295,7 +238,9 @@ void Custom::LowCmdWrite()
             }
             low_cmd.motor_cmd()[i].tau() = 0;
         }
-        switch(freemode){
+        if(set_freemode == 0){
+            set_freemode = 1;
+            switch(freemode){
             case 0:
                 freeQ = Freestandcmd(0.25, 0.0, 0, 0);  //X:14.3 0.25  Y:0 0  Z:0 0
             break;
@@ -311,6 +256,7 @@ void Custom::LowCmdWrite()
             case 4:
                 freeQ = Freestandcmd(0.10, 0.10, 0.10, 0);  //X:5.7 0.10  Y:5.7 0.10  Z:5.7 0.10
             break;
+            }
         }
     }
     else if(runing_time >= 4.0 && runing_time < 6.0)
@@ -322,13 +268,13 @@ void Custom::LowCmdWrite()
             low_cmd.motor_cmd()[i].q() = phase * freeQ[i] + (1 - phase) * stand_up_joint_pos[i];
             low_cmd.motor_cmd()[i].dq() = 0;
             if(i%3 == 0){
-                low_cmd.motor_cmd()[i].kp() = 50;
+                low_cmd.motor_cmd()[i].kp() = 55;
             }
             else if(i%3 == 1){
-                low_cmd.motor_cmd()[i].kp() = 40;
+                low_cmd.motor_cmd()[i].kp() = 55;
             }
             else if(i%3 == 2){
-                low_cmd.motor_cmd()[i].kp() = 80;
+                low_cmd.motor_cmd()[i].kp() = 85;
             }
             low_cmd.motor_cmd()[i].kd() = 5;
             if(i%3 == 2){
@@ -346,13 +292,13 @@ void Custom::LowCmdWrite()
             low_cmd.motor_cmd()[i].q() = phase * stand_up_joint_pos[i] + (1 - phase) * freeQ[i];
             low_cmd.motor_cmd()[i].dq() = 0;
             if(i%3 == 0){
-                low_cmd.motor_cmd()[i].kp() = 50;
+                low_cmd.motor_cmd()[i].kp() = 55;
             }
             else if(i%3 == 1){
-                low_cmd.motor_cmd()[i].kp() = 40;
+                low_cmd.motor_cmd()[i].kp() = 55;
             }
             else if(i%3 == 2){
-                low_cmd.motor_cmd()[i].kp() = 80;
+                low_cmd.motor_cmd()[i].kp() = 85;
             }
             low_cmd.motor_cmd()[i].kd() = 5;
             if(i%3 == 2){
@@ -370,13 +316,13 @@ void Custom::LowCmdWrite()
             low_cmd.motor_cmd()[i].q() = phase * stand_down_joint_pos[i] + (1 - phase) * stand_up_joint_pos[i];
             low_cmd.motor_cmd()[i].dq() = 0;
             if(i%3 == 0){
-                low_cmd.motor_cmd()[i].kp() = 50;
+                low_cmd.motor_cmd()[i].kp() = 55;
             }
             else if(i%3 == 1){
-                low_cmd.motor_cmd()[i].kp() = 40;
+                low_cmd.motor_cmd()[i].kp() = 55;
             }
             else if(i%3 == 2){
-                low_cmd.motor_cmd()[i].kp() = 70;
+                low_cmd.motor_cmd()[i].kp() = 85;
             }
             low_cmd.motor_cmd()[i].kd() = 5;
             if(i%3 == 2){
